cc2ced2e60cf91c5c41a4ce45de5cf4050f59b0f
Nodankumar <reddynodankumar@gmail.com>
2023-08-16 17:26:57 +0530

    Employee Payroll management

--- EmployeePayrollManagement/src/app/employee.ts
+++
@@ -1,8 +0,0 @@
-export class Employee {
-  id: number = 0;
-  name: string = '';
-  mail_id: string = '';
-  role: string = '';
-  category: string = '';
-  gender: string = '';
-}
--- EmployeePayrollManagement/src/app/leave.ts
+++
@@ -1,8 +0,0 @@
-export interface Leave {
-  //id: number;
-  employeeId: string;
-  leaveType: string;
-  startDate: string;
-  endDate: string;
-  reason: string;
-}
